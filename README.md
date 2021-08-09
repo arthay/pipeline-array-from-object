b764ea1deb38d29e384e8eb15c2b9594d07bd61b
arthay <arthur15251@gmail.com>
2021-08-10 00:09:44 +0400

    create pipeline-array-from-object

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # pipeline-array-from-object
+
+# Github Split-By Action
+
+Actions splits strings by the given delimiter
+
+## Inputs
+### `string`
+**required** string to be split
+### `split-by`
+**required** string/char to be used as the delimiter to split the string.
+
+## Output
+### value
+Object containing key value pairs
+```json
+{
+    _0: 'first',
+    _1: 'secound'
+}
+```
+
+## Example Usage
+
+```yml
+- uses: rishabhgupta/split-by@v1
+  id: split
+  with:
+    string: 'feat/branch-name'
+    split-by: '/'
+- run: | 
+    echo "${{ steps.split.outputs._1}}"
+  ```
---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+
+function run() {
+  try {
+    const inputArray = JSON.parse(core.getInput('array'));
+    const inputObject = JSON.parse(core.getInput('object'));
+    const inputEnvironment = core.getInput('environment');
+    const output = [];
+
+    for (const value of inputArray) {
+      if (inputObject[value] && inputObject[value][inputEnvironment]) {
+        output.push(inputObject[value][inputEnvironment]);
+      }
+    }
+
+    core.setOutput('outputArray', JSON.stringify(output));
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+
+}
+
+run()
