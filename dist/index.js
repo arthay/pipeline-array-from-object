15248063003c10579c4e25de510ace14a8d6c34f
arthay <arthur15251@gmail.com>
2021-08-10 00:43:39 +0400

    json parse

--- dist/index.js
+++ dist/index.js
@@ -137,18 +137,14 @@ function run() {
     const inputObject = JSON.parse(core.getInput('object'));
     const inputEnvironment = core.getInput('environment');
     const output = [];
-    const values = [];
 
     for (const value of inputArray) {
-      values.push(value);
       if (inputObject[value] && inputObject[value][inputEnvironment]) {
         output.push(inputObject[value][inputEnvironment]);
       }
     }
 
     core.setOutput('outputArray', JSON.stringify(output));
-    core.setOutput('values', JSON.stringify(values));
-    core.setOutput('inputArray', JSON.stringify(inputArray));
   } catch (err) {
     core.setFailed(err.message);
   }
