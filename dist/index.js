187eb5967624668a4ff10d7ae1f1668e729c11dc
arthay <arthur15251@gmail.com>
2021-08-10 00:31:11 +0400

    values

--- dist/index.js
+++ dist/index.js
@@ -137,14 +137,17 @@ function run() {
     const inputObject = core.getInput('object');
     const inputEnvironment = core.getInput('environment');
     const output = [];
+    const values = [];
 
     for (const value of inputArray) {
+      values.push(value);
       if (inputObject[value] && inputObject[value][inputEnvironment]) {
         output.push(inputObject[value][inputEnvironment]);
       }
     }
 
     core.setOutput('outputArray', JSON.stringify(output));
+    core.setOutput('values', JSON.stringify(values));
   } catch (err) {
     core.setFailed(err.message);
   }
